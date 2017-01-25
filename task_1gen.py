2547a34d5f408a2c0df2ac3890265f18d5953c30
Maksim Philippov <twourchin@gmail.com>
2017-01-26 00:01:48 +0700

    Full function input data generator for second stage task

--- task_1gen.py
+++ task_1gen.py
@@ -2,11 +2,14 @@
 
 import argparse
 import random
+import math
+import copy
 
 DEFAULT_WIDTH = 1024
 DEFAULT_HEIGHT = 768
 DEFAULT_SEED = 1
 DEFAULT_OUTPUT = 'all'
+DEFAULT_DELTA = 1
 
 class Point(object):
     __slots__ = ['x', 'y']
@@ -16,8 +19,10 @@ class Point(object):
         self.y = y
 
     def transform(self, alfaRad, k, cX, cY):
-        self.x = int( (self.x * math.cos(alfaRad) - self.y * math.sin(alfaRad)) * k + cX )
-        self.y = int( (self.x * math.sin(alfaRad) + self.y * math.cos(alfaRad)) * k + cY )
+        self.x = int((self.x * math.cos(alfaRad) -
+                    self.y * math.sin(alfaRad)) * k + cX )
+        self.y = int((self.x * math.sin(alfaRad) +
+                    self.y * math.cos(alfaRad)) * k + cY )
 
     def clone(self):
         return Point(self.x, self.y)
@@ -25,17 +30,23 @@ class Point(object):
     def __eq__(self, P):
         return self.x == P.x and self.y == P.y
 
+    def distanse(self, P):
+        dx = self.x - P.x if self.x > P.x else P.x - self.x
+        dy = self.y - P.y if self.y > P.y else P.y - self.y
+        return int(math.sqrt(dx**2 + dy**2))
+
+
     @staticmethod
     def random(width, height):
         return Point(
-            random.randint(1,width),
-            random.randint(1, height))
+            random.randint(-width, width),
+            random.randint(-height, height))
 
 def init(args):
-    random.seed(args.seed*
-                args.base*
-                args.width*
-                args.height*
+    random.seed(args.seed *
+                args.base *
+                args.width *
+                args.height *
                 args.ext)
 
 def output(list):
@@ -53,13 +64,39 @@ def gen_master(args):
             master.append(p)
     return master
 
-def transform_master(args, master):
+#TODO: Simulate noise on master pattern
+def randomize_master(args, master):
+    return master
 
-    pass
+def transform_master(args, transform_key, master):
+    alfaRad, k, cX, cY = transform_key
+    transform_master = copy.deepcopy(master)
+    for point in transform_master:
+        point.transform(alfaRad, k, cX, cY)
+    return transform_master
 
-def gen_recognized(args, master):
+#TODO: Add parameter for noise size multiplier
+def gen_recognized(args, transform_key, master):
+    alfaRad, k, cX, cY = transform_key
+    recognized = copy.deepcopy(master)
+    while len(recognized)<args.ext:
+        point = Point.random(args.width * 5, args.height * 5)
+        point.transform(alfaRad, k, cX, cY)
+        min_distanse = args.delta*10
+        for p in recognized:
+            distanse = p.distanse(point)
+            if distanse<min_distanse:
+                min_distanse = distanse
+        if min_distanse > args.delta:
+            recognized.insert(random.randint(0,len(recognized)-1), point)
+    return recognized
 
-    pass
+def gen_transform_key(args):
+    alfaRad = random.random()*2*math.pi
+    k = (random.random()+0.5)
+    cX = (random.random()-0.5) * args.width
+    cY = (random.random()-0.5) * args.height
+    return (alfaRad, k, cX, cY)
 
 def main():
     parser = argparse.ArgumentParser()
@@ -82,10 +119,18 @@ def main():
     parser.add_argument('--seed', type=int,
                         nargs='?',
                         default=DEFAULT_SEED,
-                        help='randomize seed for pattern generation')
+                        help='random seed for recognized (%d)' % DEFAULT_SEED)
+    parser.add_argument('--delta', type=int,
+                        nargs='?',
+                        default=DEFAULT_DELTA,
+                        help='master pattern randomize delta (%d)' % DEFAULT_DELTA)
     parser.add_argument('--output', nargs='?',
                         default=DEFAULT_OUTPUT,
-                        choices=['all', 'master', 'recognized', 'recognized-pattern', 'none'],
+                        choices=['all',
+                                 'master',
+                                 'recognized',
+                                 'recognized-pattern',
+                                 'none'],
                         help='output type (%s)' % DEFAULT_OUTPUT)
 #parse arguments
     args = parser.parse_args()
@@ -96,11 +141,17 @@ def main():
 #generate master
     master = gen_master(args)
 
+#randomize master
+    master_randomized = randomize_master(args, master)
+
+#generate transformation keys
+    transform_key = gen_transform_key(args)
+
 #transform master
-    master_transformed = transform_master(args, master)
+    master_transformed = transform_master(args, transform_key, master_randomized)
 
 #generate recognized
-    recognized = gen_recognized(args, master_transformed)
+    recognized = gen_recognized(args, transform_key, master_transformed)
 
 #output
     if args.output == 'all':
